5ccbfe298e7f7703cabe695d9318638a0a9c7870
ramondcosta <ramondiascosta@dcc.ufba.br>
2015-04-05 21:44:45 -0300

    Update TATUInterpreter.cpp
    Mudança no nome do atributo "command" para "cmd"

--- MODULOS/TPI_INTERPRETER_MOD/TATUInterpreter.cpp
+++ MODULOS/TPI_INTERPRETER_MOD/TATUInterpreter.cpp
@@ -5,56 +5,56 @@ void TATUInterpreter::Interpreter(unsigned char *string, unsigned int length){
 
     switch(string[0]){
         case COMMAND_SET:
-            command.OBJ.TYPE = TATU_SET;
+            cmd.OBJ.TYPE = TATU_SET;
 
             if(string[4] == CODE_INFO){
-                command.OBJ.CODE = TATU_CODE_INFO;
+                cmd.OBJ.CODE = TATU_CODE_INFO;
                 // Not implemented yet
             }
             else if(string[4] == CODE_STATE){
-                command.OBJ.CODE = TATU_CODE_STATE;
-                command.OBJ.PIN = ((unsigned int) string[10]) - 49;
-                command.OBJ.STATE = ((unsigned int) string[12]) - 49;
+                cmd.OBJ.CODE = TATU_CODE_STATE;
+                cmd.OBJ.PIN = ((unsigned int) string[10]) - 49;
+                cmd.OBJ.STATE = ((unsigned int) string[12]) - 49;
             }
             else break;
 
             ERROR = false;
             return;
-        case COMMAND_GET:
-            command.OBJ.TYPE = TATU_GET;
+        case cmd_GET:
+            cmd.OBJ.TYPE = TATU_GET;
             
             if(string[4] == CODE_ALL){
-                command.OBJ.CODE = TATU_CODE_ALL;
+                cmd.OBJ.CODE = TATU_CODE_ALL;
             }
             else if(string[4] == CODE_INFO){
-                command.OBJ.CODE = TATU_CODE_INFO;
+                cmd.OBJ.CODE = TATU_CODE_INFO;
                 // Not implemented yet
             }
             else if(string[4] == CODE_STATE){
-                command.OBJ.CODE = TATU_CODE_STATE;
-                command.OBJ.PIN = ((unsigned int) string[9]) - 49;
+                cmd.OBJ.CODE = TATU_CODE_STATE;
+                cmd.OBJ.PIN = ((unsigned int) string[9]) - 49;
             }
             else break;
 
             ERROR = false;
             return;
-        case COMMAND_EDIT:
-            command.OBJ.TYPE = TATU_EDIT;
+        case cmd_EDIT:
+            cmd.OBJ.TYPE = TATU_EDIT;
             if(string[5] == CODE_INFO){
-                command.OBJ.CODE = TATU_CODE_INFO;
+                cmd.OBJ.CODE = TATU_CODE_INFO;
                 // Not implemented yet
             }
             else if(string[5] == CODE_STATE){
-                command.OBJ.CODE = TATU_CODE_STATE;
-                command.OBJ.PIN = ((unsigned int) string[11]) - 49;
-                command.OBJ.STATE = ((unsigned int) string[13]) - 49;
+                cmd.OBJ.CODE = TATU_CODE_STATE;
+                cmd.OBJ.PIN = ((unsigned int) string[11]) - 49;
+                cmd.OBJ.STATE = ((unsigned int) string[13]) - 49;
             }
             else break;
 
             ERROR = false;
             return;
-        case COMMAND_POST:
-            command.OBJ.TYPE = TATU_POST;
+        case cmd_POST:
+            cmd.OBJ.TYPE = TATU_POST;
             ERROR = false;
             return;
     }
