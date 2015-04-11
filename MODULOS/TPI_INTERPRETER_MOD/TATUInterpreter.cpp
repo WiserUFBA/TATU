6f0cd64515cbdea667b5c0376769fa858adba768
Jeferson Lima <oi_je@hotmail.com>
2015-04-11 16:38:55 -0300

    Melhorado a leitura de strings
    # Modificado a forma como são armazenadas as strings.
    * Agora é possível ler Strings em hashs de 32 bits do formato DBJ2 o que possibilita a utlização de simbolos com muitos caracteres e retira a limitação anterior de 5 caracteres, além de diminuir o tamanho do objeto.
    // Tamanho final do objeto : 6 Bytes.

--- MODULOS/TPI_INTERPRETER_MOD/TATUInterpreter.cpp
+++ MODULOS/TPI_INTERPRETER_MOD/TATUInterpreter.cpp
@@ -2,17 +2,18 @@
 
 bool TATUInterpreter::parse(unsigned char *string, unsigned int length){
     int i;
+    str_hash = 5381;
+    cmd.STRUCTURE = 0;
+
     switch(string[0]){
         case COMMAND_SET:
             cmd.OBJ.TYPE = TATU_SET;
 
             if(string[4] == CODE_INFO){
                 cmd.OBJ.CODE = TATU_CODE_INFO;
-                for (i = 0; i < 6; ++i){   
-                        str_val[i] = string[9+i];
+                for(i = 9; i < length; i++){
+                    str_hash = ((str_hash << 5) + str_hash) + string[i];
                 }
-                if(i > 6) break;
-                str_val[i] = '\0';
             }
             else if(string[4] == CODE_STATE){
                 cmd.OBJ.CODE = TATU_CODE_STATE;
@@ -31,11 +32,9 @@ bool TATUInterpreter::parse(unsigned char *string, unsigned int length){
             }
             else if(string[4] == CODE_INFO){
                 cmd.OBJ.CODE = TATU_CODE_INFO;
-                for (i = 0; i < 6; ++i){   
-                        str_val[i] = string[9+i];
+                for(i = 9; i < length; i++){
+                    str_hash = ((str_hash << 5) + str_hash) + string[i];
                 }
-                if(i > 6) break;
-                str_val[i] = '\0';
             }
             else if(string[4] == CODE_STATE){
                 cmd.OBJ.CODE = TATU_CODE_STATE;
@@ -49,11 +48,9 @@ bool TATUInterpreter::parse(unsigned char *string, unsigned int length){
             cmd.OBJ.TYPE = TATU_EDIT;
             if(string[5] == CODE_INFO){
                 cmd.OBJ.CODE = TATU_CODE_INFO;
-                for (i = 0; i < 6; ++i){   
-                        str_val[i] = string[10+i];
+                for(i = 10; i < length; i++){
+                    str_hash = ((str_hash << 5) + str_hash) + string[i];
                 }
-                if(i > 6) break;
-                str_val[i] = '\0';
             }
             else if(string[5] == CODE_STATE){
                 cmd.OBJ.CODE = TATU_CODE_STATE;
