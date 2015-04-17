64c956ec33101a82851bb487ac80bdaddee3a113
jefersonla <oi_je@hotmail.com>
2015-04-16 23:26:43 -0300

    Movido os modulos para outro repositorio

--- MODULOS/SPI_MOD/spi.h
+++
@@ -1,8 +0,0 @@
-/* spi.h -- Defines SPI Communication */
-
-// Read a char from the SPI Communication
-char SPI_READ();
-// Write a char on the SPI Communication
-void SPI_WRITE(char);
-void SPI_WRITE_AT_NS(char *, char *, uint8_t);
-void SPI_WRITE_AT_NS(char *, char *, uint8_t, uint8_t);
